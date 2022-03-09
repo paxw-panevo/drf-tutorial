d67ef017311b4e8d088d49c142e2d5f9f36d1538
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-09 15:21:53 -0800

    fix: Dot notation to tutorial_config

--- tutorial_project/tutorial_config/settings.py
+++ tutorial_project/tutorial_config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'tutorial.urls'
+ROOT_URLCONF = 'tutorial_config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'tutorial.wsgi.application'
+WSGI_APPLICATION = 'tutorial_config.wsgi.application'
 
 
 # Database
