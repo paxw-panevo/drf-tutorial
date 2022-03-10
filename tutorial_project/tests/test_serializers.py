10bb92996d815d0bc2601cb76b182a04d6bf876d
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-10 15:36:04 -0800

    style: Format everything using `black`

--- tutorial_project/tests/test_serializers.py
+++ tutorial_project/tests/test_serializers.py
@@ -9,6 +9,7 @@ from pytest import fixture, mark
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
+
 @mark.django_db(transaction=True)
 def test_snippet_serializer_should_have_attributes():
     snippet = Snippet.objects.create(code='print("hello, world")\n')
@@ -17,18 +18,20 @@ def test_snippet_serializer_should_have_attributes():
     # explore the Serializers API so here's a long test!
     serializer = SnippetSerializer(snippet)
     actual_keys = list(serializer.data.keys())
-    expected_keys = ['id', 'title', 'code', 'linenos', 'language', 'style']
+    expected_keys = ["id", "title", "code", "linenos", "language", "style"]
     assert actual_keys == expected_keys
 
     actual_json = JSONRenderer().render(serializer.data)
     expected_json = (
         b'{"id":3,"title":"","code":"print(\\"hello, world\\")\\n",'
-        b'"linenos":false,"language":"python","style":"friendly"}')
+        b'"linenos":false,"language":"python","style":"friendly"}'
+    )
     assert actual_json == expected_json
 
     ### Test/Explore Deserialization process
     # Parse a stream into native datatypes
     import io
+
     stream = io.BytesIO(actual_json)
     data = JSONParser().parse(stream)
 
@@ -37,7 +40,15 @@ def test_snippet_serializer_should_have_attributes():
     assert serializer.is_valid()
 
     actual_validated_data = serializer.validated_data
-    expected_validated_data = OrderedDict([('title', ''), ('code', 'print("hello, world")'), ('linenos', False), ('language', 'python'), ('style', 'friendly')])
+    expected_validated_data = OrderedDict(
+        [
+            ("title", ""),
+            ("code", 'print("hello, world")'),
+            ("linenos", False),
+            ("language", "python"),
+            ("style", "friendly"),
+        ]
+    )
     assert actual_validated_data == expected_validated_data
 
     new_snippet = serializer.save()
--- tutorial_project/tests/test_views.py
+++ tutorial_project/tests/test_views.py
@@ -9,6 +9,7 @@ from snippets.models import Snippet
 # https://stackoverflow.com/questions/68439799/typeerror-
 # missing-1-required-positional-argument-while-using-pytest-fixture
 
+
 @mark.django_db
 class TestSnippetViews(APITestCase):
     @classmethod
@@ -17,21 +18,21 @@ class TestSnippetViews(APITestCase):
         Snippet.objects.create(code='foo = "bar"\n')
 
     def test_get_index(self):
-        response = self.client.get('/snippets/')
+        response = self.client.get("/snippets/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        actual_snippets_count = len(response.data) # type: ignore
+        actual_snippets_count = len(response.data)  # type: ignore
         self.assertEqual(actual_snippets_count, 2)
 
     def test_get_detail(self):
-        response = self.client.get('/snippets/1/')
+        response = self.client.get("/snippets/1/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        actual_data_keys = list(response.data.keys()) # type: ignore
-        expected_data_keys = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        actual_data_keys = list(response.data.keys())  # type: ignore
+        expected_data_keys = ["id", "title", "code", "linenos", "language", "style"]
         self.assertEqual(actual_data_keys, expected_data_keys)
 
     def test_endpoints_allow_format_suffix(self):
         # Let's just test a sampling
-        for url in ['/snippets/1', '/snippets']:
-            for format in ['json', 'api']:
+        for url in ["/snippets/1", "/snippets"]:
+            for format in ["json", "api"]:
                 response = self.client.get(f"{url}.{format}")
                 self.assertEqual(response.status_code, status.HTTP_200_OK)
