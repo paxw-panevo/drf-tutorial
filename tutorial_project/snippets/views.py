dbcb7c72e7e9932eb07753c2e616d13adfc2172c
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-10 15:31:20 -0800

    refactor: Use generic cbv's

--- tutorial_project/snippets/views.py
+++ tutorial_project/snippets/views.py
@@ -1,12 +1,10 @@
-from rest_framework import mixins, generics
+from rest_framework import generics
 
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 
-class SnippetList(
-    mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
-):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all snippets, or create a new snippet.
     """
@@ -14,31 +12,11 @@ class SnippetList(
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView,
-):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update, or delete a snippet instance.
     """
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
