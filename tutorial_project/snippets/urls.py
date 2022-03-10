083e0c0478a335c68a0b8557e12a1a7175355f61
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-10 15:18:21 -0800

    refactor: Use CBV

--- tutorial_project/snippets/urls.py
+++ tutorial_project/snippets/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),  # type: ignore
-    path('snippets/<int:pk>/', views.snippet_detail), # type: ignore
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
