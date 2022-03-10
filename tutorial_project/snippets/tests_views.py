8196a5beac7ec32aeb2a08ca927a7c2a4638a894
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-10 13:39:26 -0800

    test: GET endpoints of Snippets

---
+++ tutorial_project/snippets/tests_views.py
@@ -0,0 +1,23 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from snippets.models import Snippet
+
+class SnippetViewsTests(APITestCase):
+    def setUp(self) -> None:
+        Snippet.objects.create(code='foo = "bar"\n')
+        Snippet.objects.create(code='foo = "bar"\n')
+        return super().setUp()
+
+    def test_get_index(self):
+        response = self.client.get('/snippets/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        actual_snippets_count = len(response.data) # type: ignore
+        self.assertEqual(actual_snippets_count, 2)
+
+    def test_get_detail(self):
+        response = self.client.get('/snippets/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        actual_data_keys = list(response.data.keys()) # type: ignore
+        expected_data_keys = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        self.assertEqual(actual_data_keys, expected_data_keys)
