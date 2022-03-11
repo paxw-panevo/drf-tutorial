68c40ad9cd747c5edfe1024ce313c7d6676f5538
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-10 16:09:28 -0800

    feat: Add owner field to Snippet

--- tutorial_project/snippets/models.py
+++ tutorial_project/snippets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-from pygments.lexers import get_all_lexers
+from pygments import highlight
+from pygments.lexers import get_all_lexers, get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
 from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -18,6 +20,10 @@ class Snippet(models.Model):
     )
     style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
 
+    # TODO: WHY DO WE HAVE TO SPECIFY RELATED_NAME? WHAT DOES IT DO?
+    owner = models.ForeignKey(
+        "auth.User", related_name="snippets", on_delete=models.CASCADE
+    )
     highlighted = models.TextField()
 
     def save(self, *args, **kwargs):
