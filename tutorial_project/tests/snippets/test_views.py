c8828fa90869308767efa821d99ee86e1f24a312
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-11 10:34:36 -0800

    test: Use pytest API instead to be consistent
    FYI: I can't scope fixtures so it only runs once instead of every test.
    See Django Notes >"Related: pytest-django is complaining about
    how I can't use the db even though I have
    `mark.django_db(transaction=True)` mark."
    
    https://panevo.slite.com/app/docs/RuA-nJZQDiWRla

--- tutorial_project/tests/snippets/test_views.py
+++ tutorial_project/tests/snippets/test_views.py
@@ -1,37 +1,39 @@
 from django.contrib.auth.models import User
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+
+from pytest import mark, fixture
 
 from snippets.models import Snippet
 
-# TODO: Let's not mix pytest and unittest
-# https://stackoverflow.com/questions/68439799/typeerror-
-# missing-1-required-positional-argument-while-using-pytest-fixture
+
+@fixture(autouse=True)
+def create_data():
+    user, _ = User.objects.get_or_create(username="test")
+    Snippet.objects.create(code='foo = "bar"\n', owner=user)
+    Snippet.objects.create(code='foo = "bar"\n', owner=user)
 
 
-class TestSnippetViews(APITestCase):
-    @classmethod
-    def setup_class(cls):
-        user, _ = User.objects.get_or_create(username="test")
-        Snippet.objects.create(code='foo = "bar"\n', owner=user)
-        Snippet.objects.create(code='foo = "bar"\n', owner=user)
+@mark.django_db(transaction=True)
+class TestSnippetViews:
+    client = APIClient()
 
     def test_get_index(self):
         response = self.client.get("/snippets/")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert response.status_code == status.HTTP_200_OK  # type: ignore
         actual_snippets_count = len(response.data)  # type: ignore
-        self.assertEqual(actual_snippets_count, 2)
+        assert actual_snippets_count, 2
 
     def test_get_detail(self):
         response = self.client.get("/snippets/1/")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert response.status_code, status.HTTP_200_OK  # type: ignore
         actual_data_keys = list(response.data.keys())  # type: ignore
         expected_data_keys = ["id", "title", "code", "linenos", "language", "style"]
-        self.assertEqual(actual_data_keys, expected_data_keys)
+        assert actual_data_keys, expected_data_keys
 
     def test_endpoints_allow_format_suffix(self):
         # Let's just test a sampling
         for url in ["/snippets/1", "/snippets"]:
             for format in ["json", "api"]:
                 response = self.client.get(f"{url}.{format}")
-                self.assertEqual(response.status_code, status.HTTP_200_OK)
+                assert response.status_code, status.HTTP_200_OK  # type: ignore
