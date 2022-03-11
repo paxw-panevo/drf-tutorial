ef6e602b8943db9e7de5dc6a21f27e4c62bad35f
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-11 14:47:28 -0800

    feat: Create endpoint for highlighted snippets

--- tutorial_project/snippets/views.py
+++ tutorial_project/snippets/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -17,6 +17,16 @@ def api_root(request, format=None):
             "snippets": reverse("snippet-list", request=request, format=format),
         }
     )
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    # We don't want to use JSON but instead just present an HTML representation
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class SnippetList(generics.ListCreateAPIView):
