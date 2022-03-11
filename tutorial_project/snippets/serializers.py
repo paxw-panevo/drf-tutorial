912a35edca5e3bb7b2b619728791fe1cce38c27e
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-11 15:01:41 -0800

    feat: Use HyperlinkedModelSerializer

--- tutorial_project/snippets/serializers.py
+++ tutorial_project/snippets/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
 
     class Meta:
