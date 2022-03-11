b0e05f714067acc5a53d1af2815e9c4c6125e8ef
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-11 14:37:42 -0800

    fix: Remove DefaultRouter
    It's not necessary to enable the Browsable API. We didn't see the
    Browsable API at the root path before because we didn't declare a root path.

--- tutorial_project/snippets/urls.py
+++ tutorial_project/snippets/urls.py
@@ -4,20 +4,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
-router = routers.DefaultRouter()
-
-
-format_suffix_urls = format_suffix_patterns(
-    [
-        path("", views.api_root),
-        path("snippets/", views.SnippetList.as_view(), name="snippet-list"),
-        path(
-            "snippets/<int:pk>/", views.SnippetDetail.as_view(), name="snippet-detail"
-        ),
-        path("users/", views.UserList.as_view(), name="user-list"),
-        path("users/<int:pk>/", views.UserDetail.as_view(), name="user-detail"),
-    ]
-)
 urlpatterns = [
-    path("bapi/", include(router.urls)),
-] + format_suffix_urls
+    path("", views.api_root),
+    path("snippets/", views.SnippetList.as_view(), name="snippet-list"),
+    path("snippets/<int:pk>/", views.SnippetDetail.as_view(), name="snippet-detail"),
+    path("users/", views.UserList.as_view(), name="user-list"),
+    path("users/<int:pk>/", views.UserDetail.as_view(), name="user-detail"),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
