d6e78affb1a7bc13e44b54676448f9420e8f6290
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-10 16:08:50 -0800

    feat: Add highlighted field and populate on save

--- tutorial_project/snippets/models.py
+++ tutorial_project/snippets/models.py
@@ -18,5 +18,21 @@ class Snippet(models.Model):
     )
     style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
 
+    highlighted = models.TextField()
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML representation of the
+        code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = "table" if self.linenos else False
+        options = {"title": self.title} if self.title else {}
+        formatter = HtmlFormatter(
+            style=self.style, linenos=linenos, full=True, **options
+        )
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
+
     class Meta:
         ordering = ["created"]
