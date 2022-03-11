4de64a3f4d2913aad42c6b2d5eb45d7754dcf0b9
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-11 14:27:46 -0800

    feat: Implement root path

--- tutorial_project/snippets/urls.py
+++ tutorial_project/snippets/urls.py
@@ -9,10 +9,13 @@ router = routers.DefaultRouter()
 
 format_suffix_urls = format_suffix_patterns(
     [
-        path("snippets/", views.SnippetList.as_view()),
-        path("snippets/<int:pk>/", views.SnippetDetail.as_view()),
-        path("users/", views.UserList.as_view()),
-        path("users/<int:pk>/", views.UserDetail.as_view()),
+        path("", views.api_root),
+        path("snippets/", views.SnippetList.as_view(), name="snippet-list"),
+        path(
+            "snippets/<int:pk>/", views.SnippetDetail.as_view(), name="snippet-detail"
+        ),
+        path("users/", views.UserList.as_view(), name="user-list"),
+        path("users/<int:pk>/", views.UserDetail.as_view(), name="user-detail"),
     ]
 )
 urlpatterns = [
